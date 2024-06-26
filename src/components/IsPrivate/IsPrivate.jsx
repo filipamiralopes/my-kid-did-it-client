5ffec751269f7389065a3971ea2b60d5e452f321
Filipa <filipamnmlopes@hotmail.com>
2024-06-26 16:34:16 +0200

    Profile page

--- src/components/IsPrivate/IsPrivate.jsx
+++ src/components/IsPrivate/IsPrivate.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import { useContext } from "react";
+import { AuthContext } from "../../context/auth.context";
+import { Navigate } from "react-router-dom";
 
-const IsPrivate = () => {
-  return (
-    <div>IsPrivate</div>
-  )
-}
+const IsPrivate = ({ children }) => {
+  const { isLoading, isLoggedIn } = useContext(AuthContext);
 
-export default IsPrivate
+  // this is where we do the logic to check if isLoading and isLoggedIn are correct
+  if (isLoading) {
+    return <p>Is Loading...</p>;
+  }
+
+  console.log("Here is the Private Page", isLoading, isLoggedIn);
+
+  if (!isLoggedIn) {
+    return <Navigate to="/login" />;
+  }
+
+  return <div>{children}</div>;
+};
+
+export default IsPrivate;
--- src/pages/ProfilePage/ProfilePage.jsx
+++ src/pages/ProfilePage/ProfilePage.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import "./ProfilePage.css";
+import { useContext, useEffect, useState } from "react";
+import { AuthContext } from "../../context/auth.context";
+import axios from "axios";
+import {SERVER_URL} from "../../config"
 
-const ProfilePage = () => {
+function ProfilePage() {
+  const [profileUser, setProfileUser] = useState(null);
+  const { currentUser } = useContext(AuthContext);
+
+  useEffect(() => {
+    axios
+      .get(`${SERVER_URL}/auth/profile/${currentUser._id}`)
+      .then((res) => {
+        console.log("here is the profile response: ", res);
+        setProfileUser(res.data)
+      })
+      .catch((err) => console.log(err));
+  }, [currentUser]);
+
+  if(!profileUser) {
+    return <p>Loading...</p>
+  }
+
+
   return (
-    <div>ProfilePage</div>
-  )
+    <div>
+      <h1>Profile page of {profileUser?.username}</h1> 
+      <img src={profileUser?.userImage} alt={profileUser?.username} style={{ height: "250px" }}/>
+    </div>
+  );
 }
 
-export default ProfilePage
+export default ProfilePage;
+
+// profileUser && profileUser.username
