06e95235e7fb71ee337547604ed976501eeb57f2
Filipa <filipamnmlopes@hotmail.com>
2024-07-02 13:24:11 +0200

    worked on canvas appeareance

--- src/pages/CanvasPage/CanvasPage.jsx
+++ src/pages/CanvasPage/CanvasPage.jsx
@@ -5,6 +5,8 @@ import { AuthContext } from "../../context/auth.context";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import { SERVER_URL } from "../../config";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUndo, faRedo, faRotate } from "@fortawesome/free-solid-svg-icons";
 
 const styles = {
   border: "0.0625rem solid #9c9c9c",
@@ -15,6 +17,9 @@ function CanvasPage() {
   const { currentUser } = useContext(AuthContext);
   const [title, setTitle] = useState("");
   const [error, setError] = useState(null);
+  const [strokeColor, setStrokeColor] = useState("#000000");
+  const [canvasColor, setCanvasColor] = useState("#FFFFFF");
+
   const canvas = useRef();
   const nav = useNavigate();
 
@@ -22,40 +27,97 @@ function CanvasPage() {
   const handleUploadDrawing = (e) => {
     e.preventDefault();
 
-    if(!title) {
-      setError("Give a title to your kiddo's work of art")
+    if (!title) {
+      setError("Give a title to your kiddo's work of art");
     }
 
-    canvas.current
-      .exportImage("png")
-      .then((file) => {
-        const requestBody = { title, author:currentUser._id, drawingData: file};
-        axios
-          .post(`${SERVER_URL}/api/drawings/upload`, requestBody)
-          .then(() => {
-            nav("/drawings");
-            console.log("Drawing uploaded!");
-          })
-          .catch((err) => console.log(err));
-      })
+    canvas.current.exportImage("png").then((file) => {
+      const requestBody = { title, author: currentUser._id, drawingData: file };
+      axios
+        .post(`${SERVER_URL}/api/drawings/upload`, requestBody)
+        .then(() => {
+          nav("/drawings");
+          console.log("Drawing uploaded!");
+        })
+        .catch((err) => console.log(err));
+    });
+  };
+
+  const handleStrokeColorChange = (e) => {
+    setStrokeColor(e.target.value);
+  };
+
+  const handleCanvasColorChange = (e) => {
+    setCanvasColor(e.target.value);
+  };
+
+  const handleResetClick = () => {
+    setCanvasColor("#FFFFFF");
+    canvas.current?.resetCanvas();
+  };
+
+  const handleUndoClick = () => {
+    canvas.current?.undo();
+  };
+
+  const handleRedoClick = () => {
+    canvas.current?.redo();
   };
 
   return (
-    <div className="canvas-container">
-      <h1>Draw here!</h1>
-      <ReactSketchCanvas
-        ref={canvas}
-        style={styles}
-        width="350px"
-        height="350px"
-        canvasColor="transparent"
-        strokeColor="#a855f7"
-      />
-      <label>Title:</label>
-      <input type="text" name="title" value={title} onChange={handleTitle} />
-      <button onClick={handleUploadDrawing}>Save</button>
-      {error && <p style={{ color: "red" }}>{error}</p>}
-    </div>
+    <>
+      <div className="canvas-container">
+        <ReactSketchCanvas
+          ref={canvas}
+          style={styles}
+          width="350px"
+          height="350px"
+          strokeColor={strokeColor}
+          canvasColor={canvasColor === "#FFFFFF" ? "transparent" : canvasColor}
+        />
+        <div className="canvas-tools">
+          <label htmlFor="color"></label>
+          <input
+            className="stroke-color"
+            type="color"
+            value={strokeColor}
+            onChange={handleStrokeColorChange}
+          />
+          <label htmlFor="color"></label>
+          <input
+            className="canvas-color"
+            type="color"
+            value={canvasColor}
+            onChange={handleCanvasColorChange}
+          />
+          <button
+            type="button"
+            title="Undo"
+            className="btn btn-sm btn-outline-primary"
+            onClick={handleUndoClick}
+          >
+            <FontAwesomeIcon icon={faUndo} />
+          </button>
+          <button type="button" title="Redo" onClick={handleRedoClick}>
+            <FontAwesomeIcon icon={faRedo} />
+          </button>
+          <button type="button" title="Clear" onClick={handleResetClick}>
+            <FontAwesomeIcon icon={faRotate} />
+          </button>
+          <div className="title-save">
+            <label>Title:</label>
+            <input
+              type="text"
+              name="title"
+              value={title}
+              onChange={handleTitle}
+            />
+            <button onClick={handleUploadDrawing}>Save</button>
+            {error && <p style={{ color: "red" }}>{error}</p>}
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
