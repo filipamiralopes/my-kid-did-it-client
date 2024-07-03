f485d3bac38bbee507a091e8bce4229ccbc6bed2
Filipa <filipamnmlopes@hotmail.com>
2024-07-03 16:15:21 +0200

    worked on order card

--- src/components/DrawingCard/DrawingCard.jsx
+++ src/components/DrawingCard/DrawingCard.jsx
@@ -10,8 +10,18 @@ import { useNavigate } from "react-router-dom";
 import { AuthContext } from "../../context/auth.context";
 import axios from "axios";
 import { SERVER_URL } from "../../config";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faDownload,
+  faTrash,
+  faCartShopping,
+} from "@fortawesome/free-solid-svg-icons";
 
-export default function Drawingcard({ drawing, setCurrentOrder, handleDelete }) {
+export default function Drawingcard({
+  drawing,
+  setCurrentOrder,
+  handleDelete,
+}) {
   const { currentUser } = useContext(AuthContext);
   const parsedDate = new Date(drawing.createdAt);
   const nav = useNavigate();
@@ -36,7 +46,10 @@ export default function Drawingcard({ drawing, setCurrentOrder, handleDelete }) 
   };
 
   return (
-    <Card sx={{ maxWidth: 260, width: 300 }} id="drawing-card">
+    <Card
+      sx={{ maxWidth: 300, width: 300, maxHeight: 300, p: 1 }}
+      id="drawing-card"
+    >
       <CardMedia
         component="img"
         alt={`${drawing.title} drawing`}
@@ -44,27 +57,31 @@ export default function Drawingcard({ drawing, setCurrentOrder, handleDelete }) 
         image={drawing.file}
       />
       <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
+        <Typography gutterBottom variant="h5" component="div" align="left">
           {drawing.title}
         </Typography>
-        <Typography variant="body2" color="text.secondary">
-          {`Added on ${parsedDate.toDateString()}`}
+        <Typography variant="body2" color="text.secondary" align="left">
+          {`CREATED ON ${parsedDate.toDateString().toUpperCase().slice(3)}`}
         </Typography>
       </CardContent>
-      <CardActions>
-        <Button size="small" onClick={handleChooseProduct}>
-          Order
+      <CardActions sx={{ display: "inline-flex" }}>
+        <Button size="medium" onClick={handleChooseProduct}  style={{ fontSize: '18px' }} title="Order">
+          <FontAwesomeIcon icon={faCartShopping} />
         </Button>
-        <Button size="small">
+        <Button size="small" title="Download">
           <a
             href={drawing.file.replace("/upload/", "/upload/fl_attachment/")}
             download
           >
-            Download
+            <Typography variant="body2" color="#1876D1" fontSize="18px">
+              <FontAwesomeIcon icon={faDownload} />
+            </Typography>
           </a>
         </Button>
-        <Button onClick={() => handleDelete(drawing._id)}>
-          Delete
+        <Button onClick={() => handleDelete(drawing._id)} title="Trash">
+          <Typography variant="body2" color="#C40617" fontSize="18px">
+            <FontAwesomeIcon icon={faTrash} />
+          </Typography>
         </Button>
       </CardActions>
     </Card>
