236283a1dcccd29815adc8405bec0f5c59d6dafe
Filipa <filipamnmlopes@hotmail.com>
2024-07-04 14:03:47 +0200

    homepage WIP

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -4,12 +4,27 @@ import { useNavigate } from "react-router-dom";
 const HomePage = () => {
   const nav = useNavigate();
   return (
-    <div>
-      <h1>My Kid Did It</h1>
-      <h3>Custom Printing</h3>
-      <button onClick={() => nav("/canvas")}>Start drawing!</button>
-    </div>
-  )
-}
+    <>
+      <div className="home-page-container">
+        <div className="headers">
+          <h1>Customize</h1>
+          <p>
+            Make your kid's masterpieces unforgettable <br />
+            with custom prints
+          </p>
+        </div>
+        <div className="carousel">
+          <h1>
+            CAROUSEL <br />
+            PLACEHOLDER
+          </h1>
+        </div>
+      </div>
+      <div>
+        <button onClick={() => nav("/canvas")}>Start drawing!</button>
+      </div>
+    </>
+  );
+};
 
-export default HomePage
+export default HomePage;
