4b79c95379a1b4bce67a9123183d57f72399865c
Filipa <filipamnmlopes@hotmail.com>
2024-07-04 12:34:41 +0200

    basic homepage

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,12 +1,13 @@
-import React from 'react'
-import { Link } from "react-router-dom";
+import "./HomePage.css";
+import { useNavigate } from "react-router-dom";
 
 const HomePage = () => {
+  const nav = useNavigate();
   return (
     <div>
       <h1>My Kid Did It</h1>
-      <h3>Custom embroidery</h3>
-      <Link to="/canvas"><h5>Start drawing!</h5></Link>
+      <h3>Custom Printing</h3>
+      <button onClick={() => nav("/canvas")}>Start drawing!</button>
     </div>
   )
 }
