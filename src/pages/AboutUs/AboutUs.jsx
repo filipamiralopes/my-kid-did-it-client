e76a707eb47b1f2ae20f599828275cdf81ed107f
Filipa <filipamnmlopes@hotmail.com>
2024-07-05 01:25:48 +0200

    About Us done

--- src/pages/AboutUs/AboutUs.jsx
+++ src/pages/AboutUs/AboutUs.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import usImg from "../../assets/us.png";
 
 const AboutUs = () => {
   return (
-    <div>AboutUs</div>
-  )
-}
+    <div className="home-page-container">
+      <div className="headers">
+        <h1>About Us</h1>
+        <p>
+          Teacher, leave them kids alone! <br />
+          All in all, it's just another brick in the wall.
+        </p>
+      </div>
+      <div className="carousel">
+        <img src={usImg} alt="founders picture" style={{ width: "330px" }} />
+      </div>
+    </div>
+  );
+};
 
-export default AboutUs
+export default AboutUs;
